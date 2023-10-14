08ba31ce3d9b0495ebd892c37c118bb478953ddb
kkyouma <144501527+kkyouma@users.noreply.github.com>
2023-10-14 18:37:16 -0300

    add computer random selection

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+
 function getComputerChoice() {
-    
+//crear variable que contenga todas las opciones
+    const options = ["rock", "paper", "scissors"];
+//se elije una posibilidad
+    const randomSelection = Math.floor(Math.random() * 3);
+    const choice = options[randomSelection];
+// entregar la posibilidad
+    return choice
 }
+
