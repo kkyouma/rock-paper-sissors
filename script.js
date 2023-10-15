39b421c69ac03461073ff8585fbe454009eafe4b
kkyouma <144501527+kkyouma@users.noreply.github.com>
2023-10-15 16:35:19 -0300

    add round function

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 
 function getComputerChoice() {
-//crear variable que contenga todas las opciones
     const options = ["rock", "paper", "scissors"];
-//se elije una posibilidad
     const randomSelection = Math.floor(Math.random() * 3);
     const choice = options[randomSelection];
-// entregar la posibilidad
-    return choice
+    return choice;
 }
+
+function playRound(playerSelection, computerSelection) {
+    const win = "You win!";
+    const lose = "You lose";
+    const draw = "Its a draw";
+
+    if (playerSelection === computerSelection) {
+        return draw 
+
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "rock")
+    ) {
+        return win
+
+    } else {
+        return lose
+    }
+}
+const computerSelection = getComputerChoice();
+const playerSelection = "rock";
+let result = playRound(playerSelection, computerSelection);
 
