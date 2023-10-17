7dac9503e03afb74f92f6faadb6a2a231a6dba58
Juan Manuel Cabezas Contardo <juanmanuelcontardo@gmail.com>
2023-10-16 22:19:04 -0300

    refactor(script.js):manage of exception options

--- script.js
+++ script.js
@@ -25,6 +25,21 @@ function playRound(playerSelection, computerSelection) {
         return lose
     }
 }
+function validate(player){
+    if(player==="rock"){
+        console.log(1)
+        return 1
+    }else if(player==="paper"){
+        console.log(1)
+        return 1
+    }else if(player==="scissors"){
+        console.log(1)
+        return 1
+    }else{
+        console.log(0)
+        return 0
+    }
+}
 
 function game() {
 
@@ -32,10 +47,17 @@ function game() {
     let computerScore = 0
 
     for (round = 1; round <= 5; round++) {
-        const playerSelection = prompt("Choose: rock, paper or scissors").toLowerCase();
+        try{
+            let playerSelection = prompt("Choose: rock, paper or scissors").toLowerCase();
+            while ( validate(playerSelection) == 0 ){
+                playerSelection = prompt("Choose: rock, paper or scissors").toLowerCase();
+        }
+        }catch(error){
+            console.log(error);
+        }
+
         const computerSelection = getComputerChoice();
         const result = playRound(playerSelection, computerSelection);
-
         console.log("Round: " + round);
         console.log("Player chose: " + playerSelection);
         console.log("Computer chose: " + computerSelection);
